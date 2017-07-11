bf1a00239dfcc74c52aa96b0b871dcd6831adaeb
PAFCZuCrew <mpandel@zulily.com>
2017-07-11 08:38:24 -0400

    Add files via upload

---
+++ associateList.js
@@ -0,0 +1,5 @@
+function populateList(){
+    for (i = 0; i < 200; i++) {     
+        document.write('<div id="associate"><table><tr id="tableText"><td><img id="associateListImage" src="Images/A_Default.jpg"></td><td>Michael Pandel |</td></tr></table></div>')
+    }
+}
